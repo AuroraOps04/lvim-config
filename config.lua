27642cdc501d573fef15e91230c473d34d098765
AuroraOps04 <5239604@qq.com>
2024-04-11 18:43:17 +0800

    init commit

---
+++ config.lua
@@ -0,0 +1,48 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+lvim.transparent_window = true
+require "./keymap"
+require "./options"
+lvim.plugins = {
+  { "craftzdog/solarized-osaka.nvim" },
+  {
+    "zbirenbaum/copilot-cmp",
+    event = "InsertEnter",
+    dependencies = { "zbirenbaum/copilot.lua" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup()     -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+        require("copilot_cmp").setup() -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+      end, 100)
+    end,
+  },
+  {
+    "aserowy/tmux.nvim",
+    config = function()
+      require("plugins.tmux")
+    end
+  },
+  {
+    "ggandor/leap.nvim",
+    name = "leap",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+    },
+    opts = {
+      debug = false, -- Enable debugging
+      proxy = "http://127.0.0.1:7890"
+      -- See Configuration section for rest
+    },
+  },
+}
+lvim.colorscheme = "solarized-osaka"
---
+++ lua/options.lua
@@ -0,0 +1,33 @@
+local opt          = vim.opt
+opt.relativenumber = true
+opt.number         = true
+opt.clipboard      = "unnamedplus"
+opt.fileencoding   = "utf-8"
+opt.hlsearch       = true
+opt.showtabline    = 2
+opt.smartcase      = true
+opt.smartindent    = true
+opt.swapfile       = false
+opt.termguicolors  = true
+-- 等待一个键映射的时间 单位 ms
+opt.timeoutlen     = 800
+opt.undofile       = true
+-- 更快的完成速度
+opt.updatetime     = 300
+opt.writebackup    = false
+opt.wrap           = false
+-- enable tab => space
+opt.expandtab      = true
+opt.shiftwidth     = 2
+opt.tabstop        = 2
+opt.cursorline     = true
+opt.numberwidth    = 4
+-- 显示左侧符号栏
+opt.signcolumn     = "yes"
+opt.scrolloff      = 5
+opt.sidescrolloff  = 5
+opt.spell          = false
+opt.showmode       = false
+-- 配置搜索忽略大小写
+opt.ignorecase     = true
+opt.clipboard      = "unnamedplus"
